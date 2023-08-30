bc7ab9dcd85b18a83e9de0abea64901cdb7d7361
Jonathan_Leder <jonyleder@gmail.com>
2023-08-30 20:47:56 -0300

    Trabajo para revision

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,44 @@
-let currentIndex = 0;
+const slider = document.querySelector("#slider");
+let sliderSection= document.querySelectorAll(".sliderSection");
+let sliderSectionLast= sliderSection[sliderSection.length-1];
 
-function changeSlide(n) {
-    currentIndex += n;
-    const slides = document.querySelectorAll(".slide");
-    
-    if (currentIndex >= slides.length) {
-        currentIndex = 0;
-    }
-    
-    if (currentIndex < 0) {
-        currentIndex = slides.length - 1;
-    }
-    
-    slides.forEach(slide => slide.classList.remove("active"));
-    slides[currentIndex].classList.add("active");
+const btnIz = document.querySelector("#btnIz");
+const btnDer = document.querySelector("#btnDer");
+
+slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+
+function movDer(){
+    let sliderSectionFirst= document.querySelectorAll(".sliderSection")[0];
+    slider.style.marginLeft= "-200%";
+    slider.style.transition= "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition= "none";
+        slider.insertAdjacentElement('beforeend',sliderSectionFirst);
+        slider.style.marginLeft="-100%";
+    },500);
 }
+
+function movIz(){
+    let sliderSection= document.querySelectorAll(".sliderSection");
+    let sliderSectionLast= sliderSection[sliderSection.length-1];
+
+    slider.style.marginLeft= "0%";
+    slider.style.transition= "all 0.5s"; // Agrega 's' a la duración
+    setTimeout(function(){
+        slider.style.transition= "none";
+        slider.insertAdjacentElement('afterbegin',sliderSectionLast); // Cambia sliderSectionFirst por sliderSectionLast
+        slider.style.marginLeft="-100%";
+    },500);
+}
+
+btnDer.addEventListener('click',function(){
+    movDer();
+});
+
+btnIz.addEventListener('click',function(){
+    movIz();
+});
+
+setInterval(function(){
+    movDer();
+},5000);
